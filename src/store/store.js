b667806e91500ddf7a201bfb524173d66cb58aae
Mohamed Helmy <mtabila37@gmail.com>
2019-03-27 16:36:14 +0200

    Handle store -Delete Compose func

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,18 @@
 
-import { createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import {
     createReduxContainer,
     createReactNavigationReduxMiddleware,
 } from 'react-navigation-redux-helpers';
-import logger from 'redux-logger';
-import {thunk} from 'redux-thunk';
 import { connect } from 'react-redux';
 
 import AppNavigator from '../navigation/AppNavigator';
 import appReducer from './reducers/appReducer';
 
-let composeEnhancers = compose;
 
 const middleware = [];
 
-middleware.push(thunk);
-
 middleware.push(createReactNavigationReduxMiddleware(state => state.nav ));
-
-if (__DEV__) {
-    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-}
 
 const App = createReduxContainer(AppNavigator);
 
@@ -29,4 +20,4 @@ const mapStateToProps = state => ({ state: state.nav });
  
 export const AppWithNavigationState = connect(mapStateToProps)(App);
 
-export const store = createStore(appReducer, composeEnhancers(applyMiddleware(...middleware)));
+export const store = createStore(appReducer, applyMiddleware(...middleware));
